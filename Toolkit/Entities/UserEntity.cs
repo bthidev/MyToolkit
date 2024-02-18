4a6617bf194cec6d64696f7a910674163aaee3e6
speedi <thiebaut.benjamin@live.fr>
2024-02-18 01:42:13 +0100

    rollback automapper

--- Toolkit/Entities/UserEntity.cs
+++ Toolkit/Entities/UserEntity.cs
@@ -5,7 +5,7 @@ using ToolKit.Entities;
 namespace Toolkit.Entities
 {
     [Index(nameof(Email), IsUnique = true)]
-    public class UserEntity: EntityBase
+    public class UserEntity : EntityBase
     {
         [Required]
         public string Email { get; set; }
