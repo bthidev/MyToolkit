a3eca0b35bf4526bfc39aed147c039f9a6fc4318
thidev <thiebaut.benjamin@live.fr>
2024-06-03 00:30:38 +0200

    fixe missing id

--- Toolkit/Entities/DtoBase.cs
+++ Toolkit/Entities/DtoBase.cs
@@ -1,11 +1,9 @@
 using System;
-using System.Text.Json.Serialization;
 
 namespace ToolKit.Entities
 {
     public abstract class DtoBase
     {
-        [JsonPropertyName("Id")]
         public Guid Id { get; set; }
     }
 }
